7aac0a0896ce5ea2aff5bbcd4dd8b3a7be738ff1
William Wu <william@pylabs.org>
2017-10-24 01:33:35 +0800

    let mysql can be reconfigured

--- files/configure_percona.sh
+++ files/configure_percona.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-mysqladmin -uroot password "$1"
+# see https://www.percona.com/blog/2016/05/18/where-is-the-mysql-5-7-root-password/ and
+# https://dev.mysql.com/doc/refman/5.7/en/resetting-permissions.html for more info
+systemctl stop mysql.service
+echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$1';" > /tmp/reset_mysql
+mysqld --init-file=/tmp/reset_mysql &
+sleep 5
+killall mysqld
+sleep 5
+systemctl start mysql.service
 mysql -uroot -p"$1" <<<'DROP DATABASE IF EXISTS test'
 touch /tmp/configure_percona_done
